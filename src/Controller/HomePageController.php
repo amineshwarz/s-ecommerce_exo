178a2862cb5d35b43c5f4c2b0e1698421caa5a3b
amineshwarz <elkhal.medamine@gmail.com>
2025-08-05 00:13:36 +0200

    page d'index avec modal et page produit

--- src/Controller/HomePageController.php
+++ src/Controller/HomePageController.php
@@ -2,17 +2,29 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Product;
+use App\Repository\ProductRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 final class HomePageController extends AbstractController
 {
     #[Route('/', name: 'app_home_page', methods: ['GET'])]
-    public function index(): Response
+    public function index(ProductRepository $productRepository): Response
     {
+        
         return $this->render('home_page/index.html.twig', [
             'controller_name' => 'HomePageController',
+            'products' => $productRepository->findAll(),
+        ]);
+    }
+
+    #[Route('/product/{id}/show', name: 'app_home_product_show', methods: ['GET'])]
+    public function show(Product $product): Response
+    {
+        return $this->render('home_page/produit.html.twig', [
+            'product' => $product,
         ]);
     }
 }
